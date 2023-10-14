e40b2241f19a727f39db03cd4a20168df1586eb2
JuergenReiser <juergen.reiser@neo-q.de>
2023-10-14 13:23:32 +0200

    Add main layout + navigation menu

---
+++ GitHubNavigator/Shared/MainLayout.razor.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace GitHubNavigator.Shared;
+
+public partial class MainLayout
+{
+    private readonly MudTheme _theme = new();
+    private bool _isDrawerOpen = true;
+
+    private void ToggleDrawer()
+    {
+        _isDrawerOpen = !_isDrawerOpen;
+    }
+}
