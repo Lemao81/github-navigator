af4d93f8aab34c15ba160502be56a104015e9afa
JuergenReiser <juergen.reiser@neo-q.de>
2023-10-15 08:52:57 +0200

    Add dark mode toggle

--- GitHubNavigator/Shared/MainLayout.razor.cs
+++ GitHubNavigator/Shared/MainLayout.razor.cs
@@ -6,9 +6,9 @@ public partial class MainLayout
 {
     private readonly MudTheme _theme = new();
     private bool _isDrawerOpen = true;
+    private bool _isDarkMode = false;
 
-    private void ToggleDrawer()
-    {
-        _isDrawerOpen = !_isDrawerOpen;
-    }
+    private void ToggleDrawer() => _isDrawerOpen = !_isDrawerOpen;
+
+    private void ToggleDarkMode() => _isDarkMode = !_isDarkMode;
 }
