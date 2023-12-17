8b1dda1e9feb13cab561ebe2a3eecefd1bb22daa
JuergenReiser <juergen.reiser@neo-q.de>
2023-12-17 12:53:09 +0100

    Add user search list

--- GitHubNavigator/Pages/SearchUsers.razor.cs
+++ GitHubNavigator/Pages/SearchUsers.razor.cs
@@ -1,4 +1,6 @@
-﻿using GitHubNavigator.Extensions;
+﻿using System.Collections.ObjectModel;
+using GitHubNavigator.Domain.Models;
+using GitHubNavigator.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace GitHubNavigator.Pages;
@@ -7,6 +9,11 @@ public partial class SearchUsers
 {
     private string AccessToken { get; set; } = "";
     private string SearchText { get; set; } = "";
+    private List<UserViewModel> _usersTotal = new();
+    private ObservableCollection<UserViewModel> _users = new();
+    private int _totalPageCount = 1;
+    private int _currentPage = 1;
+    private const int PageSize = 12;
 
     private async Task OnKeyDownAsync(KeyboardEventArgs args)
     {
@@ -24,6 +31,34 @@ public partial class SearchUsers
         if (!result.IsSuccess)
         {
             Snackbar.ShowError(result.Error);
+
+            return;
         }
+
+        _totalPageCount = (int)Math.Ceiling(result.Value!.Count / (double)PageSize);
+        _usersTotal.Clear();
+        _usersTotal.AddRange(result.Value!);
+        UpdateUsersPage(1);
+    }
+
+    private void UpdateUsersPage(int page)
+    {
+        if (_usersTotal.Count < PageSize)
+        {
+            _currentPage = 1;
+            _users = new ObservableCollection<UserViewModel>(_usersTotal);
+
+            return;
+        }
+
+        if (page < 1)
+        {
+            _currentPage = 1;
+            _users = new ObservableCollection<UserViewModel>(_usersTotal.Take(PageSize));
+
+            return;
+        }
+
+        _users = new ObservableCollection<UserViewModel>(_usersTotal.Skip((page - 1) * PageSize).Take(PageSize));
     }
 }
